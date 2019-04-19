5ab5479891d79a4982d838f9bd7ac1b57f9679a9
Ethan Silvas <ethansilvas2@gmail.com>
2019-04-19 14:02:42 -0700

    set up routes file that App uses

---
+++ src/routes/Routes.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const DisplayPage = () => (
+    <Router>
+        <div>
+            HI
+        </div>
+    </Router>
+);
+
+export default DisplayPage;
