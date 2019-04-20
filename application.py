6b39d35e13e5f89b31135149c0958de240988514
Wes Gray <wesg@uw.edu>
2019-04-19 17:41:10 -0700

    uploading flask stuff

---
+++ application.py
@@ -0,0 +1,22 @@
+import os
+
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask_sqlalchemy import SQLAlchemy
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "bookdatabase.db"))
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+
+db = SQLAlchemy(app)
+
+@app.route('/', methods=["GET", "POST"])
+def home():
+    if request.form:
+        print(request.form)
+    return render_template("index.html")
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ template.py
@@ -0,0 +1,7 @@
+from flask import render_template, Flask
+app = Flask(__name__)
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('index.html', name=name)
